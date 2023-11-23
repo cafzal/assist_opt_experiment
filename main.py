3f621ebc84350ceef23677494808899aa54d5190
cafzal <cameron.afzal@gmail.com>
2023-11-23 21:46:38 +0000

    update app

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from openai import OpenAI
-import pymoo as pymoo
+import openai
+# import pymoo as pymoo
 import os
 
 st.title("Optimization Assistant")
@@ -10,55 +10,105 @@ st.write(
 
 st.header("Step 1: Define the problem")
 
-# Add drop down menu for problem area
-problem_area = st.selectbox(
-    "Select a problem area:",
-    ["Energy", "Water", "Transportation", "Manufacturing", "Supply Chain"])
-
-# Add drop down menu for problem type
-problem_type = st.selectbox("Select a problem type:",
-                            ["Strategic Planning", "Operations"])
-
-# Add checkboxes for optimization objectives
-objectives = st.multiselect(
-    "Select optimization objectives:",
-    ["Minimize Cost", "Maximize Benefits", "Minimize Risk"])
+# Initialize the OpenAI client
+client = openai.OpenAI()
 
 # Setup OpenAI assistant to guide user through optimization problem
-client = OpenAI()
-assistant = client.beta.assistants.create(
-    name="Optimization Assistant",
-    instructions=
-    "You are an expert at leveraging multi-objective optimization to improve planning and operations of complex networks. You will help domain experts define problems, select methods, run, and interpret results.",
-    tools=[{
-        "type": "code_interpreter"
-    }],
-    model="gpt-3.5-turbo-1106")
+# assistant = client.beta.assistants.create(
+#     name="Optimization Assistant",
+#     instructions=
+#     "You are an expert at leveraging multi-objective optimization to improve planning and operations of complex networks. You will help domain experts define problems, select methods, run, and interpret results.",
+#     tools=[{
+#         "type": "code_interpreter"
+#     }],
+#     model="gpt-3.5-turbo-1106")
+assistant_id = "asst_60dvjUYk1TZJuNgVFp48bKAO"
 
-st.button("Start")
-if st.button:
-  # Generate assistant response based on user input
-  thread = client.beta.threads.create()
-  message = client.beta.threads.messages.create(
-      thread_id=thread.id,
-      role="user",
-      content=
-      f"Help define a {problem_area} {problem_type} problem where we aim to {objectives}."
-  )
 
-  run = client.beta.threads.runs.create(thread_id=thread.id,
-                                        assistant_id=assistant.id,
-                                        instructions=".")
+# Function to display problem definition widgets
+def define_problem():
+  problem_area = st.selectbox(
+      "Select a problem area:",
+      ["Energy", "Water", "Transportation", "Manufacturing", "Supply Chain"],
+      key='problem_area')
 
-# Click button to display assistant response
-st.button("Get response")
-if st.button and 'thread' in locals():
-  run = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
-  messages = client.beta.threads.messages.list(thread_id=thread.id)
+  problem_type = st.selectbox("Select a problem type:",
+                              ["Strategic Planning", "Operations"],
+                              key='problem_type')
 
-  # Display message response to user
-  if messages:
-    st.write(messages)
+  objectives = st.multiselect(
+      "Select optimization objectives:",
+      ["Minimize Cost", "Maximize Benefits", "Minimize Risk"],
+      key='objectives')
+  return problem_area, problem_type, objectives
+
+
+# Start session button
+if st.button("Start Session"):
+  # Create a thread and store in session state
+  # Uncomment the following line and comment out the placeholder when integrating with OpenAI
+  # thread = client.beta.threads.create()
+  # st.session_state['thread_id'] = thread.id
+  st.session_state[
+      'thread_id'] = "thread_LkRzVY7uFm3OyjV9NYbVqQ2u"  # Placeholder thread ID
+
+# Display the problem definition menus and get the values
+problem_area, problem_type, objectives = define_problem()
+
+# Define problem and get response button
+if st.button("Define Problem and Get Response"):
+  if 'thread_id' in st.session_state:
+    # Send message to thread
+    message = client.beta.threads.messages.create(
+        thread_id=st.session_state['thread_id'],
+        role="user",
+        content=
+        f"Help define a {problem_area} {problem_type} problem where we aim to {', '.join(objectives)}."
+    )
+
+    # Create and execute a run
+    run = client.beta.threads.runs.create(
+        thread_id=st.session_state['thread_id'],
+        assistant_id=assistant_id,
+        instructions=
+        "The user is a domain expert. Please help them define and solve their optimization problem."
+    )
+
+    # Store run ID in session state
+    st.session_state['run_id'] = run.id
+
+# # Display the problem definition menus regardless of button states
+# define_problem()
+
+# Display response button
+if st.button("Display Response"):
+  if 'thread_id' in st.session_state and 'run_id' in st.session_state:
+    # Retrieve the run to get the response
+    run = client.beta.threads.runs.retrieve(
+        thread_id=st.session_state['thread_id'],
+        run_id=st.session_state['run_id'])
+
+    # List messages in the thread
+    messages = client.beta.threads.messages.list(
+        thread_id=st.session_state['thread_id'])
+
+    # Check if messages are present
+    if messages:
+      # Filter out only assistant's messages
+      assistant_messages = [msg for msg in messages if msg.role == 'assistant']
+
+      # Get the latest assistant message
+      if assistant_messages:
+        latest_message = assistant_messages[0]  # Last message in the list
+        message_content = latest_message.content if latest_message.content else 'No content available'
+
+        # Display the message content
+        st.write("Assistant's Response:")
+        st.write(message_content)
+      else:
+        st.write("No responses from the assistant found.")
+    else:
+      st.write("No messages found in the thread.")
 
 # # User can upload csv file
 # uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"])
